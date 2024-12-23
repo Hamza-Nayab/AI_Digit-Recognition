2bcab8143449c64b49a12be237f7d6c021940056
Hamza-Nayab <Hamza.nayab48@gmail.com>
2024-12-23 00:02:01 -0800

    Made model

---
+++ main.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+# Load and preprocess the MNIST dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = tf.keras.utils.normalize(x_train.astype('float32'), axis=1)
+x_test = tf.keras.utils.normalize(x_test.astype('float32'), axis=1)
+
+# Define the model
+model = tf.keras.models.Sequential([
+    tf.keras.Input(shape=(28, 28)),  # Input layer
+    tf.keras.layers.Flatten(),      # Flatten 2D images into 1D vectors
+    tf.keras.layers.Dense(128, activation='relu'),  # Hidden layer 1
+    tf.keras.layers.Dense(128, activation='relu'),  # Hidden layer 2
+    tf.keras.layers.Dense(10, activation='softmax') # Output layer
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=3)
+
+# Save and load the model
+model.save('handwritten.keras')  
+#model = tf.keras.models.load_model('handwritten.keras')  
+
+
+# Evaluate the model
+# loss, accuracy = model.evaluate(x_test, y_test)
+# print(f"Loss: {loss}")
+# print(f"Accuracy: {accuracy}")
